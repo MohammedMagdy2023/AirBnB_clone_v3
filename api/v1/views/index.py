806ba56156a5433700359c21a5532f54f0960268
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-02-24 10:13:10 -0800

    update index.pyapi/v1/views/index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,9 +7,11 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
+
 @app_views.route("/status")
 def api():
     return jsonify({"status": "OK"})
+
 
 @app_views.route("/stats")
 def stats():
