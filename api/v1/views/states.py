7a7b77589c7b744e1f140701dbfebedb1552d56f
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-02-25 17:41:21 +0200

    stats

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,4 +1,17 @@
+#!/usr/bin/python3
+"""
+Define an api route for stats to get, post and delete stats from the database
+"""
+
 from flask import request
 
-if request.method == "POST":
-    pass
+
+@app_views.route("/stats")
+def stats():
+    classes = {"Amenity": "amenities", "City": "cities",
+               "Place": "places", "Review": "reviews",
+               "State": "states", "User": "users"}
+    new_dict = {}
+    for key, value in classes.items():
+        new_dict[value] = storage.count(key)
+    return jsonify(new_dict, 200)
