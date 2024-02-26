dc852d0d80acc24bab825cc238649d38910751cb
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-02-26 10:32:08 -0800

    test

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,33 +4,29 @@ The API routes for the RESTFUL API
 """
 
 from api.v1.views import app_views
-from flask import jsonify, request
+from flask import jsonify
 from models import storage
 
 
 @app_views.route("/status", strict_slashes=False)
 def api():
     """
-    test the api status 
+    test the status of the API
     """
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+@app_views.route("/stats", strict_slashes=False)
 def stats():
-    """
-    function to return the count of all class objects
-    """
-    if request.method == 'GET':
-        response = {}
-        classes = {
-            "Amenity": "amenities",
-            "City": "cities",
-            "Place": "places",
-            "Review": "reviews",
-            "State": "states",
-            "User": "users"
-        }
-        for key, value in classes.items():
-            response[value] = storage.count(key)
-        return jsonify(response)
+    classes = {
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+        "User": "users"
+    }
+    new_dict = {}
+    for key, value in classes.items():
+        new_dict[value] = storage.count(key)
+    return jsonify(new_dict)
