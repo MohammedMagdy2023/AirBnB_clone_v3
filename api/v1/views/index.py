eca287a35335a783a45e70677fe2a27c18611fd4
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-02-25 16:49:21 -0800

    strict_slash

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,16 +8,21 @@ from flask import jsonify
 from models import storage
 
 
-@app_views.route("/status")
+@app_views.route("/status", strict_slashes=False)
 def api():
     return jsonify({"status": "OK"})
 
 
-@app_views.route("/stats")
+@app_views.route("/stats", strict_slashes=False)
 def stats():
-    classes = {"Amenity": "amenities", "City": "cities",
-               "Place": "places", "Review": "reviews",
-               "State": "states", "User": "users"}
+    classes = {
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+        "User": "users"
+}
     new_dict = {}
     for key, value in classes.items():
         new_dict[value] = storage.count(key)
