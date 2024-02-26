5ca1e093ec4bb2a5853ae1ca4929f7d85fd77e6f
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-02-25 16:40:14 -0800

    states operations

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,24 +1,71 @@
 #!/usr/bin/python3
-"""
-Define an api route for stats to get, post and delete stats from the database
-"""
+"""states.py"""
 
-from flask import request, jsonify
 from api.v1.views import app_views
+from flask import abort, jsonify, make_response, request
 from models import storage
+from models.state import State
 
 
-@app_views.route('/states/', methods=['GET', 'POST', 'DELETE'])
+@app_views.route('/states', strict_slashes=False)
 def states():
     """
     Return a list of all stats in the database
     """
-    if request.method == 'GET':
-        dict = storage.all('State')
-        return jsonify([obj.to_dict() for obj in dict.values()])
-    elif request.method == 'POST':
-        return jsonify({'message': 'POST /stats'})
-    elif request.method == 'DELETE':
-        return jsonify({'message': 'DELETE /stats'})
-    else:
-        return jsonify({'message': 'Not F** found'}), 404
+    states = storage.all('State')
+    return jsonify([state.to_dict() for state in states.values()])
+
+
+@app_views.route('/states/<string:state_id>', methods=['GET'],
+                 strict_slashes=False)
+def state_by_id(state_id):
+    """
+    Return a list of all stats in the database where id is equal to state_id
+    """
+    try:
+        state = storage.get(State, state_id)
+        return jsonify(state.to_dict())
+    except Exception:
+        abort(404)
+
+
+@app_views.route('/states/<string:state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_state(state_id):
+    """deletes a state based on its state_id"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    state.delete()
+    storage.save()
+    return (jsonify({}))
+
+
+@app_views.route('/states/', methods=['POST'], strict_slashes=False)
+def create_state():
+    """
+    Create A new record in the datebase
+    """
+    if not request.get_json():
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+    if 'name' not in request.get_json():
+        return make_response(jsonify({'error': 'Missing name'}), 400)
+    state = State(**request.get_json())
+    state.save()
+    return make_response(jsonify(state.to_dict()), 201)
+
+
+@app_views.route('/states/<string:state_id>', methods=['PUT'],
+                 strict_slashes=False)
+def update_state(state_id):
+    """update a state"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    if not request.get_json():
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+    for key, value in request.get_json().items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(state, key, value)
+    state.save()
+    return jsonify(state.to_dict())
