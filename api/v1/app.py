de692a4abb542141bf284d1e890a2296986c6fb5
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2024-02-26 21:16:58 +0200

    Add CORS support

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@ The Base api app instance for the Airbnb project
 """
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 import os
@@ -11,6 +12,8 @@ import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
