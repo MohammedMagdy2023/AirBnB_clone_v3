10122a1c27152a5eba63368b685a331fd61794cf
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-02-26 14:19:06 -0800

    edit __init____init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,9 +2,6 @@
 """Blueprint initialization."""
 
 from flask import Blueprint
-
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-
 from .amenities import *
 from .cities import *
 from .index import *
@@ -12,3 +9,5 @@ from .places import *
 from .places_reviews import *
 from .states import *
 from .users import *
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
